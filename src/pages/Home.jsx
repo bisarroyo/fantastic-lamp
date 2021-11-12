67775b97da87c16d63db4f6ff0b87ba498a4c17a
bisarroyo <65843187+bisarroyo@users.noreply.github.com>
2021-11-11 18:44:22 -0600

    Chántese

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import TextArea from '../components/TextArea';
-import ButtonPrimary from '../components/ButtonPrimary';
-import ButtonSecondary from '../components/ButtonSecondary';
-import BoxArea from '../components/BoxArea';
-import Payments from '../components/Payments';
-import Faq from '../components/Faq';
+import React from "react";
+import TextArea from "../components/TextArea";
+import ButtonPrimary from "../components/ButtonPrimary";
+import ButtonSecondary from "../components/ButtonSecondary";
+import BoxArea from "../components/BoxArea";
+import Payments from "../components/Payments";
+import Faq from "../components/Faq";
 
 const Home = () => {
   return (
     <div>
       <TextArea />
-      <ButtonPrimary text='Start' />
-      <ButtonSecondary text='logout' />
-      <BoxArea backgroundColor='#ac3' text='hola, esto es una prueba' />
-      <BoxArea backgroundColor='#acf' text='hola, esto es una prueba' />
-      <BoxArea backgroundColor='#fca' text='hola, esto es una prueba' />
-      <BoxArea backgroundColor='#aaa' text='hola, esto es una prueba' />
-      <Payments title='Ultimos pagos ralizados' />
+      <ButtonPrimary text="Start" />
+      <ButtonSecondary text="logout" />
+      <BoxArea
+        backgroundColor="#ac3"
+        text="hola, esto es una prueba de formato"
+        size="100px"
+      />
+      <BoxArea backgroundColor="#acf" text="hola, esto es una prueba" />
+      <BoxArea backgroundColor="#fca" text="hola, esto es una prueba" />
+      <BoxArea backgroundColor="#aaa" text="hola, esto es una prueba" />
+      <Payments title="Ultimos pagos ralizados" />
       <Faq />
     </div>
   );
